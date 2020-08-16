54f585015f4fff681d7dd46441484618265d0d8a
kimdonghyun <kdh8677@naver.com>
2020-08-16 17:00:47 +0900

    200816A StartServer 추가

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,8 +1,11 @@
 package application;
 
+import java.net.InetSocketAddress;
 import java.net.ServerSocket;
+import java.net.Socket;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
+import java.util.concurrent.Executors;
 
 import javafx.application.Application;
 import javafx.stage.Stage;
@@ -15,8 +18,38 @@ public class Main extends Application {
 	ServerSocket serverSocket;
 	
 	// 서버를 구동시켜 클라이언트의 연걸을 기다리는 메소드
-	public void startServer(String IP, int port){
-		
+	public void startServer(String IP, int port) {
+		try {
+			serverSocket = new ServerSocket();
+			serverSocket.bind(new InetSocketAddress(IP, port));
+		} catch (Exception e) {
+			e.printStackTrace();
+			if (!serverSocket.isClosed())
+				stopServer();
+			return;
+		}
+
+		// 클라이언트가 접속할때까지 기다리는 쓰레드
+
+		Runnable thread = new Runnable() {
+			@Override
+			public void run() {
+				while (true) {
+					try {
+						Socket socket = serverSocket.accept();
+						clients.add(new Client(socket));
+						System.out.println("[클라이언트 접속]" + socket.getRemoteSocketAddress() + ": "
+								+ Thread.currentThread().getName());
+					} catch (Exception e) {
+						if (!serverSocket.isClosed())
+							stopServer();
+						break;
+					}
+				}
+			}
+		};
+		threadPool = Executors.newCachedThreadPool();
+		threadPool.submit(thread);
 	}
 	
 	// 서버 작동 중지 메소드
