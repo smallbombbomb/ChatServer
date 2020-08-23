2e6d0a023a18ca584efa77816a5994de4f83ac40
kimdonghyun <kdh8677@naver.com>
2020-08-23 14:16:04 +0900

    200823_ 소켓프로그램 서버/클라이언트 연동

--- src/application/Main.java
+++ src/application/Main.java
@@ -1,13 +1,24 @@
 package application;
 
+//import java.awt.Font;
+//import java.awt.TextArea;
 import java.net.InetSocketAddress;
 import java.net.ServerSocket;
 import java.net.Socket;
+import java.util.Iterator;
 import java.util.Vector;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
+
 import javafx.application.Application;
+import javafx.application.Platform;
+import javafx.geometry.Insets;
+import javafx.scene.Scene;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextArea;
+import javafx.scene.layout.BorderPane;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class Main extends Application {
@@ -54,13 +65,67 @@ public class Main extends Application {
 	
 	// 서버 작동 중지 메소드
 	public void stopServer() {
-		
+		try {
+			// 현 작동중인 모든 소켓 닫기
+			Iterator<Client> iterator = clients.iterator();
+			while (iterator.hasNext()) {
+				Client client = iterator.next();
+				client.socket.close();
+				iterator.remove();
+			}
+			// 서버 소켓 객체 닫기
+			if(serverSocket != null && !serverSocket.isClosed())
+				serverSocket.close();
+			// 쓰레드풀 종료
+			if(threadPool != null && !threadPool.isShutdown())
+				threadPool.shutdown();
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
 	}
 	
-	// UI 생성하고 실질적 프로그램 작동 메소드
+	// UI 생성하고 실질적 프로그램 작동 메소드 (GUI)
 	@Override
 	public void start(Stage primaryState){
+		BorderPane root = new BorderPane();
+		root.setPadding(new Insets(5));
 		
+		TextArea textArea = new TextArea();
+		textArea.setEditable(false);
+		textArea.setFont(new Font("나눔고딕", 15));
+		root.setCenter(textArea);
+		
+		Button toggleButton = new Button("시작하기");
+		toggleButton.setMaxWidth(Double.MAX_VALUE);
+		BorderPane.setMargin(toggleButton, new Insets(1, 0, 0, 0));
+		root.setBottom(toggleButton);
+		
+		String IP = "127.0.0.1";
+		int port = 9876;
+		
+		toggleButton.setOnAction(event -> {
+			if(toggleButton.getText().equals("시작하기")) {
+				startServer(IP, port);
+				Platform.runLater(() -> {
+					String message = String.format("[서버 시작]\n", IP, port);
+					textArea.appendText(message); 
+					toggleButton.setText("종료하기");
+				});
+			}else{
+				stopServer();
+				Platform.runLater(() -> {
+					String message = String.format("[서버 종료]\n", IP, port);
+					textArea.appendText(message);
+					toggleButton.setText("시작하기");
+				});
+			}
+		});
+		
+		Scene scene = new Scene(root, 400, 400);
+		primaryState.setTitle("[ 채팅 서버 ]");
+		primaryState.setOnCloseRequest(event->stopServer());
+		primaryState.setScene(scene);
+		primaryState.show();
 	}
 	
 	// 프로그램의 진입점
